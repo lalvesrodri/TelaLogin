0e3dfc430723dd1dfbc223c86a7e15a3178f9c49
Lucas Barros <luveslucas@yahoo.com.br>
2020-07-22 23:10:50 -0300

    perfil

--- src/components/Postagem/index.js
+++ src/components/Postagem/index.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "./style.css";
 import Users from "../../pages/amigos/users";
 import perfil from "../../components/fotos/ft_entrega1.jpg";
+import Foto from "../../components/fotos/01-12-17-97548520_156917345868286_6086438138105222836_n.jpg";
 
 class Postagem extends React.Component {
   render() {
@@ -12,19 +13,22 @@ class Postagem extends React.Component {
       <div className="card-container">
         <div className="titulo">
           <li>
-            <img src="perfil" />{" "}
+            <img src={perfil} width="30" />{" "}
           </li>
           <li>FLUXO</li>
-          <div className="Postagem">
-            <img
-              className="user-picture"
-              //src={this.props.data.foto}
-              alt="Estudante"
-            />
+          <div className="Postagem" width="150">
+            <img src={Foto} />
           </div>
-          <div className="imagens inferior"></div>
+          <img
+          //className="user-picture"
+          //src={this.props.data.foto}
+          //alt="Estudante"
+          />
         </div>
+        <div className="imagens inferior"></div>
+        <div className="comentario"></div>
       </div>
+
       //</Link>
     );
   }
